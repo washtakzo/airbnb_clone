aadfd5a5163915ef0cfd2a5b6323352def9d5979
washtakzo <washtakzo@gmail.com>
2022-10-06 13:50:21 +0200

    v1 progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import ProgressBar from "@badrap/bar-of-progress";
+import React from "react";
+import Router from "next/router";
 
+const progressBar = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-100",
+  delay: 200,
+});
+Router.events.on("routeChangeStart", progressBar.start);
+Router.events.on("routeChangeComplete", progressBar.finish);
+Router.events.on("routeChangeError", progressBar.finish);
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
